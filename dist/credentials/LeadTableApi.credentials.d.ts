66cf1c9c7d7924c1444f1d9fe373b3a172be7f50
root <info@najah-solutions.de>
2025-08-22 18:42:34 +0200

    🎉 Initial commit

--- dist/credentials/LeadTableApi.credentials.d.ts
+++
@@ -1,8 +0,0 @@
-import { IAuthenticateGeneric, ICredentialTestRequest, ICredentialType, INodeProperties } from 'n8n-workflow';
-export declare class LeadTableApi implements ICredentialType {
-    name: string;
-    displayName: string;
-    properties: INodeProperties[];
-    authenticate: IAuthenticateGeneric;
-    test: ICredentialTestRequest;
-}
--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { LeadTable } from './nodes/LeadTable/LeadTable.node';
-import { LeadTableTrigger } from './nodes/LeadTable/LeadTableTrigger.node';
-import { LeadTableApi } from './credentials/LeadTableApi.credentials';
-export declare const nodes: (typeof LeadTable | typeof LeadTableTrigger)[];
-export declare const credentials: (typeof LeadTableApi)[];
